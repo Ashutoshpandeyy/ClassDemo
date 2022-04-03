b688b7429cd1a58fc6590a110da1212fbe2cbabe
Ashutoshpandeyy <starpandey1998@gmail.com>
2022-04-03 15:05:58 +0530

    upd

--- FJP6-main/module_2/web_Scrapping/leaderboard.js
+++ FJP6-main/module_2/web_Scrapping/leaderboard.js
@@ -1,65 +1,84 @@
-const  request = require("request");
+const request = require("request");
 const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const fs = require("fs");
 
-const {JSDOM} = jsdom;
 const link = "https://www.espncricinfo.com/series/ipl-2021-1249214/match-results";
 
 let leaderboard = [];
+let counter = 0;
+
 request(link,cb);
 
-function cb(error, response,html){
+function cb(error,response,html){
     if(error){
         console.log(error);
     }else{
         const dom = new JSDOM(html);
         const document = dom.window.document;
-        let allScoreCardTags = document.querySelectorAll('a[data-hover="Scorecard"]');
-        for(let i =0; i<allScoreCardTags.length;i++){
-            let link = allScoreCardTags[i].href;
-            let completelink =  "https://www.espncricinfo.com" + link;
-            // console.log(completelink);
-            request(completelink,cb2)
+        let allScorecardTags = document.querySelectorAll('a[data-hover="Scorecard"]');
+        for(let i=0;i<allScorecardTags.length;i++){
+            let link = allScorecardTags[i].href;
+            let completeLink = "https://www.espncricinfo.com"+link;
+            // console.log(completeLink);
+            request(completeLink,cb2);
+            counter++;
         }
+        // console.log("Line 24: ",leaderboard);
     }
 }
-function cb2(error, response ,html){
+
+function cb2(error,response,html){
     if(error){
         console.log(error);
     }else{
         const dom = new JSDOM(html);
-        const  document = dom.window.document;
-        let batsmanrow = document.querySelectorAll(".table.batsman tbody tr");
-        for(let i = 0; i<batsmanrow.length; i++){
-            let cells = batsmanrow[i].querySelectorAll("td");
-            if(cells.length==8){
+        const document = dom.window.document;
+        let batsmenRow = document.querySelectorAll(".table.batsman tbody tr");
+        for(let i=0;i<batsmenRow.length;i++){
+            let cells = batsmenRow[i].querySelectorAll("td");
+            if(cells.length == 8){
                 let name = cells[0].textContent;
                 let runs = cells[2].textContent;
                 let balls = cells[3].textContent;
                 let fours = cells[5].textContent;
                 let sixes = cells[6].textContent;
+                // console.log("Name : ",name,"Runs : ",runs,"Balls : ",balls,"Fours : ",fours,"Sixes : ",sixes);
+                processPlayer(name,runs,balls,fours,sixes);
             }
+        }
+        counter--;
+        if(counter == 0){
+            console.log(leaderboard);
+            let data = JSON.stringify(leaderboard);
+            fs.writeFileSync('BatsmenStats.json',data);
+
+
         }
     }
 }
 
-processPlayer('Rohit','15','4','2','4');
-processPlayer('Virat','50','20','4','3');
-processPlayer('Rohit','40','20','1','2');
 function processPlayer(name,runs,balls,fours,sixes){
-    runs = Number[runs];
-    balls = Number[balls];
-    fours = Number[fours];
-    sixes = Number[sixes];
-    for(let i =0; i<leaderboard.length; i++){
-        let playerobj = leaderboard[i];
-        playerobj.Runs +=runs;
-        playerobj.Balls +=balls;
-        playerobj.Fours +=fours;
-        playerobj.Sixes +=sixes;
-        return;
+    runs = Number(runs);
+    balls = Number(balls);
+    fours = Number(fours);
+    sixes = Number(sixes);
+    for(let i=0;i<leaderboard.length;i++){
+        let playerObj = leaderboard[i];
+        if(playerObj.Name == name){
+            //will do some work here
+            playerObj.Runs+=runs;
+            playerObj.Innings+=1;
+            playerObj.Balls+=balls;
+            playerObj.Fours+=fours;
+            playerObj.Sixes+=sixes;
+            return;
+        }
     }
+    // code coming here means we did not find our player inside leaderboard
     let obj = {
-        Name :name,
+        Name:name,
+        Innings:1,
         Runs:runs,
         Balls:balls,
         Fours:fours,
